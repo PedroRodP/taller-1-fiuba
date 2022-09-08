201088870bc432afc689146a64a8117fc85b9088
Pedro Rodríguez Peluffo <ppedro.rod@gmail.com>
2022-09-07 22:27:14 -0300

    Versión que compila e imprime algo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::cmp::max;
 use std::cmp::min;
 use std::env;
 use std::fs;
+use std::mem::replace;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,11 +11,11 @@ fn main() {
         .expect("Error leyendo el archivo");
     let input_bytes = content.as_bytes();
     let mut buscaminas = armar_buscaminas(input_bytes);
-    let buscaminas = buscar_minas_adyacentes(buscaminas);
-    imprimir_tablero(buscaminas);
+    let tablero = buscar_minas_adyacentes(&mut buscaminas);
+    imprimir_tablero(tablero);
 }
 
-fn imprimir_tablero(tablero : Vec::<Vec::<&str>>) {
+fn imprimir_tablero(tablero : &Vec::<Vec::<String>>) {
     for fila in tablero {
         for columna in fila {
             print!("{}", columna)
@@ -23,54 +24,55 @@ fn imprimir_tablero(tablero : Vec::<Vec::<&str>>) {
     }
 }
 
-fn buscar_minas_adyacentes(buscaminas : Vec::<Vec::<&str>>) -> Vec::<Vec::<&str>> {
+fn buscar_minas_adyacentes(buscaminas : &mut Vec::<Vec::<String>>) -> &mut Vec::<Vec::<String>> {
     let fila_size = buscaminas.len();
     let columna_size = buscaminas[0].len();
     for fila in 0..(fila_size - 1) {
         for columna in 0..(columna_size) {
             if buscaminas[fila][columna] == "*" {
-                incrementar_adyacentes(&buscaminas, fila, columna);
+                incrementar_adyacentes(buscaminas, fila, columna);
             }
         }
     }
     buscaminas
 }
 
-fn incrementar_adyacentes(buscaminas : &Vec::<Vec::<&str>>, fila : usize, columna : usize) {
-    let fila_desde = max(0, fila - 1);
+fn incrementar_adyacentes(buscaminas : &mut Vec::<Vec::<String>>, fila : usize, columna : usize) {
+    let fila_desde = max(0, fila as isize - 1) as usize;
     let fila_hasta = min(buscaminas.len(), fila + 1);
-    let columna_desde = max(0, columna - 1);
+    let columna_desde = max(0, columna as isize - 1) as usize;
     let columna_hasta = min(buscaminas[0].len(), columna + 1);
     // Recorro el cuadrado que redodea la mina, cuidadosamente de no salir de los límites
     for f in fila_desde..fila_hasta {
         for c in columna_desde..columna_hasta {
-            let mut casilla = &mut *buscaminas[f][c];
-            if casilla != "*" {
-                if casilla == "." { //Inicializar casillas adyacentes
-                    *casilla = "0";
+            let casilla = buscaminas.get_mut(f).unwrap().get_mut(c).unwrap();
+            if *casilla != "*".to_string() {
+                if *casilla == ".".to_string() { //Inicializar casillas adyacentes
+                    *casilla = "0".to_string();
                 }
                 let mut pos_int : i64 = casilla.parse().unwrap();
                 pos_int = pos_int + 1;
-                *casilla = &pos_int.to_string();
+                *casilla = pos_int.to_string();
+                //replace(casilla, pos_int.to_string());
             }
         }
     }
 }
 
-fn armar_buscaminas(input_bytes: &[u8]) -> Vec::<Vec::<&str>> {
-    let mut buscaminas = Vec::<Vec::<&str>>::new();
+fn armar_buscaminas(input_bytes: &[u8]) -> Vec::<Vec::<String>> {
+    let mut buscaminas = Vec::<Vec::<String>>::new();
     let mut fila = 0;
-    let vector = Vec::<&str>::new();
+    let vector = Vec::<String>::new();
     buscaminas.push(vector);
     for &byte in input_bytes {
         match byte {
             10 => { // 10 = ASCII para LF
                 fila = fila + 1; // Un salto de línea significa una nueva fila
-                let vector = Vec::<&str>::new();
+                let vector = Vec::<String>::new();
                 buscaminas.push(vector);
             },
-            42 => buscaminas[fila].push("*"), // 42 = ASCII para .
-            46 => buscaminas[fila].push("."), // 46 = ASCII para *
+            42 => buscaminas[fila].push("*".to_string()), // 42 = ASCII para .
+            46 => buscaminas[fila].push(".".to_string()), // 46 = ASCII para *
             _ => panic!("Caracter inválido en el buscaminas")
         }
     }
