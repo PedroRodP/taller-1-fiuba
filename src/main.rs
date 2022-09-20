f2a72ad7eecb193a2015ef6be6c6f85f31c6063b
Pedro Rodríguez Peluffo <ppedro.rod@gmail.com>
2022-09-19 22:43:28 -0300

    * While para lectura de archivo removido * Strings reemplazados por bins * Static vars para los bins (sino era ilegible)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::cmp::max;
 use std::cmp::min;
 use std::env;
 use std::fs;
-use std::io::stdin;
+
+static VACIO : u8 = b'.';
+static MINA : u8 = b'*';
+static CERO : u8 = b'0';
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,58 +14,39 @@ pub fn main() {
 }
 
 pub fn procesar_buscaminas(ruta_archivo: &String) {
-    let contenido_archivo = fs::read_to_string(ruta_archivo);
-    let mut archivo_no_encontrado = true;
-    while archivo_no_encontrado {
-        solicitar_correccion_archivo(&contenido_archivo, ruta_archivo, &mut archivo_no_encontrado);
-    }
-    resolver_buscaminas(contenido_archivo.unwrap());
-}
-
-fn solicitar_correccion_archivo(
-    contenido_archivo: &Result<String, std::io::Error>,
-    ruta_archivo: &String,
-    archivo_no_encontrado: &mut bool,
-) {
-    if contenido_archivo.is_err() {
-        println!("No se encontró el archivo {}. Revise que se encuentre en la ruta especificada y presione enter para reintentar.", ruta_archivo);
-        stdin()
-            .read_line(&mut String::new())
-            .expect("Error inesperado en stdin");
-    } else {
-        *archivo_no_encontrado = false;
-    }
+    let contenido_archivo = fs::read_to_string(ruta_archivo).expect("Error abriendo el archivo");
+    resolver_buscaminas(contenido_archivo);
 }
 
 fn resolver_buscaminas(file_read: String) {
     let input_bytes = file_read.as_bytes();
     let mut buscaminas = armar_buscaminas(input_bytes);
-    let tablero = buscar_minas_adyacentes(&mut buscaminas);
-    imprimir_tablero(tablero);
+    buscar_minas_adyacentes(&mut buscaminas);
+    imprimir_tablero(&buscaminas);
 }
 
-fn armar_buscaminas(input_bytes: &[u8]) -> Vec<Vec<String>> {
-    let mut buscaminas = Vec::<Vec<String>>::new();
+fn armar_buscaminas(input_bytes: &[u8]) -> Vec<Vec<u8>> {
+    let mut buscaminas = Vec::<Vec<u8>>::new();
     let mut fila = 0;
-    let vector = Vec::<String>::new();
+    let vector = Vec::<u8>::new();
     buscaminas.push(vector);
     for &byte in input_bytes {
         match byte {
             10 => {
                 // 10 = ASCII para LF
                 fila = fila + 1; // Un salto de línea significa una nueva fila
-                let vector = Vec::<String>::new();
+                let vector = Vec::<u8>::new();
                 buscaminas.push(vector);
             }
-            42 => buscaminas[fila].push("*".to_string()), // 42 = ASCII para .
-            46 => buscaminas[fila].push(".".to_string()), // 46 = ASCII para *
+            42 => buscaminas[fila].push(MINA), // 42 = ASCII para .
+            46 => buscaminas[fila].push(VACIO), // 46 = ASCII para *
             _ => panic!("Caracter inválido en el buscaminas"),
         }
     }
     buscaminas
 }
 
-fn buscar_minas_adyacentes(buscaminas: &mut Vec<Vec<String>>) -> &mut Vec<Vec<String>> {
+fn buscar_minas_adyacentes(buscaminas: &mut Vec<Vec<u8>>) {
     let fila_size = buscaminas.len();
     let columna_size = buscaminas[0].len();
     for fila in 0..fila_size {
@@ -72,14 +56,13 @@ fn buscar_minas_adyacentes(buscaminas: &mut Vec<Vec<String>>) -> &mut Vec<Vec<St
             }
         }
     }
-    buscaminas
 }
 
-fn es_una_mina(casilla: &String) -> bool {
-    casilla == "*"
+fn es_una_mina(casilla: &u8) -> bool {
+    *casilla == MINA
 }
 
-fn incrementar_adyacentes(buscaminas: &mut Vec<Vec<String>>, fila: usize, columna: usize) {
+fn incrementar_adyacentes(buscaminas: &mut Vec<Vec<u8>>, fila: usize, columna: usize) {
     // Casteo a isize porque las restas pueden dar -1
     let fila_desde = max(0, fila as isize - 1) as usize;
     let fila_hasta = min(buscaminas.len() - 1, fila + 1) + 1;
@@ -100,24 +83,20 @@ fn incrementar_adyacentes(buscaminas: &mut Vec<Vec<String>>, fila: usize, column
     }
 }
 
-fn incrementar_casilla(casilla: &mut String) {
-    if casilla == "." {
+fn incrementar_casilla(casilla: &mut u8) {
+    if *casilla == VACIO {
         //Inicializar casillas adyacentes aún no inicializadas
-        *casilla = "0".to_string();
+        *casilla = CERO;
     }
-    let mut casilla_int: usize = casilla
-        .parse()
-        .expect("La casilla a incrementar no es numérica ni vacía");
-    casilla_int = casilla_int + 1;
-    *casilla = casilla_int.to_string();
+    *casilla = *casilla + 1;
 }
 
-fn imprimir_tablero(tablero: &Vec<Vec<String>>) {
+fn imprimir_tablero(tablero: &Vec<Vec<u8>>) {
     for fila in tablero {
         for columna in fila {
-            print!("{} ", columna)
+            print!("{} ", char::from(*columna));
         }
-        println!()
+        println!();
     }
 }
 
@@ -131,15 +110,15 @@ mod tests {
         let buscaminas = armar_buscaminas(input_bytes);
         assert_eq!(buscaminas.len(), 3);
         assert_eq!(buscaminas[0].len(), 3);
-        assert_eq!(buscaminas[0][0], "*");
-        assert_eq!(buscaminas[0][1], ".");
-        assert_eq!(buscaminas[0][2], ".");
-        assert_eq!(buscaminas[1][0], ".");
-        assert_eq!(buscaminas[1][1], ".");
-        assert_eq!(buscaminas[1][2], "*");
-        assert_eq!(buscaminas[2][0], ".");
-        assert_eq!(buscaminas[2][1], "*");
-        assert_eq!(buscaminas[2][2], ".");
+        assert_eq!(buscaminas[0][0], MINA);
+        assert_eq!(buscaminas[0][1], VACIO);
+        assert_eq!(buscaminas[0][2], VACIO);
+        assert_eq!(buscaminas[1][0], VACIO);
+        assert_eq!(buscaminas[1][1], VACIO);
+        assert_eq!(buscaminas[1][2], MINA);
+        assert_eq!(buscaminas[2][0], VACIO);
+        assert_eq!(buscaminas[2][1], MINA);
+        assert_eq!(buscaminas[2][2], VACIO);
     }
 
     #[test]
@@ -152,54 +131,54 @@ mod tests {
     #[test]
     fn busca_minas_adyacentes_correctamente() {
         let mut buscaminas = vec![
-            vec!["*".to_string(), ".".to_string(), ".".to_string()],
-            vec![".".to_string(), ".".to_string(), "*".to_string()],
-            vec![".".to_string(), "*".to_string(), ".".to_string()],
+            vec![MINA, VACIO, VACIO],
+            vec![VACIO, VACIO, MINA],
+            vec![VACIO, MINA, VACIO],
         ];
-        let resultado = buscar_minas_adyacentes(&mut buscaminas);
-        assert_eq!(resultado[0][0], "*");
-        assert_eq!(resultado[0][1], "2");
-        assert_eq!(resultado[0][2], "1");
-        assert_eq!(resultado[1][0], "2");
-        assert_eq!(resultado[1][1], "3");
-        assert_eq!(resultado[1][2], "*");
-        assert_eq!(resultado[2][0], "1");
-        assert_eq!(resultado[2][1], "*");
-        assert_eq!(resultado[2][2], "2");
+        buscar_minas_adyacentes(&mut buscaminas);
+        assert_eq!(buscaminas[0][0], MINA);
+        assert_eq!(buscaminas[0][1], b'2');
+        assert_eq!(buscaminas[0][2], b'1');
+        assert_eq!(buscaminas[1][0], b'2');
+        assert_eq!(buscaminas[1][1], b'3');
+        assert_eq!(buscaminas[1][2], MINA);
+        assert_eq!(buscaminas[2][0], b'1');
+        assert_eq!(buscaminas[2][1], MINA);
+        assert_eq!(buscaminas[2][2], b'2');
     }
 
     #[test]
     fn incrementa_adyacentes_correctamente() {
         let mut buscaminas = vec![
-            vec!["*".to_string(), ".".to_string(), ".".to_string()],
-            vec![".".to_string(), ".".to_string(), "*".to_string()],
-            vec![".".to_string(), "*".to_string(), ".".to_string()],
+            vec![MINA, VACIO, VACIO],
+            vec![VACIO, VACIO, MINA],
+            vec![VACIO, MINA, VACIO],
         ];
         let fila = 2;
         let columna = 1;
         incrementar_adyacentes(&mut buscaminas, fila, columna);
-        assert_eq!(buscaminas[0][0], "*");
-        assert_eq!(buscaminas[0][1], ".");
-        assert_eq!(buscaminas[0][2], ".");
-        assert_eq!(buscaminas[1][0], "1");
-        assert_eq!(buscaminas[1][1], "1");
-        assert_eq!(buscaminas[1][2], "*");
-        assert_eq!(buscaminas[2][0], "1");
-        assert_eq!(buscaminas[2][1], "*");
-        assert_eq!(buscaminas[2][2], "1");
+        assert_eq!(buscaminas[0][0], MINA);
+        assert_eq!(buscaminas[0][1], VACIO);
+        assert_eq!(buscaminas[0][2], VACIO);
+        assert_eq!(buscaminas[1][0], b'1');
+        assert_eq!(buscaminas[1][1], b'1');
+        assert_eq!(buscaminas[1][2], MINA);
+        assert_eq!(buscaminas[2][0], b'1');
+        assert_eq!(buscaminas[2][1], MINA);
+        assert_eq!(buscaminas[2][2], b'1');
     }
 
     #[test]
     fn incrementa_casilla_correctamente() {
-        let mut casilla = "1".to_string();
+        let mut casilla = b'1';
         incrementar_casilla(&mut casilla);
-        assert_eq!(casilla, "2");
+        assert_eq!(casilla, b'2');
     }
 
     #[test]
     fn inicializa_casilla_en_cero_correctamente_y_la_incrementa() {
-        let mut casilla = ".".to_string();
+        let mut casilla = VACIO;
         incrementar_casilla(&mut casilla);
-        assert_eq!(casilla, "1");
+        assert_eq!(casilla, b'1');
     }
 }
